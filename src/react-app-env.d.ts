aeef3a39e3eaa4f3f72d63ab7cc307e428f12b7d
James Lefrère <james@colony.io>
2020-03-04 10:21:59 +0100

    Add `.env.example`

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,8 +4,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test'
     PUBLIC_URL: string
-    REACT_APP_HASH: string
-    REACT_APP_API_URI: string
-    REACT_APP_WS_URI: string
+    REACT_APP_FORGE_REWARDS_ADDRESS: string
+    REACT_APP_GRAPHQL_ENDPOINT: string
+    REACT_APP_GRAPHQL_ENDPOINT_WS: string
   }
 }
