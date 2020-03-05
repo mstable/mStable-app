72f995e80c24cdf1158c3b4a2df185eca77bf115
James Lefrère <james@colony.io>
2020-03-05 15:34:59 +0100

    Add string utils
    * Add `getEtherscanLink` util from the dashboard
    * Add `truncateAddress` util

--- src/web3/strings.ts
+++ src/web3/strings.ts
@@ -1,1 +1,30 @@
-export { asciiToHex as aToH } from 'web3-utils'
+export { asciiToHex as aToH } from 'web3-utils';
+
+const ETHERSCAN_PREFIXES = {
+  1: '',
+  3: 'ropsten.',
+  4: 'rinkeby.',
+  5: 'goerli.',
+  42: 'kovan.',
+};
+
+export const getEtherscanLink = (
+  chainId: number,
+  data: string,
+  type: string,
+): string => {
+  const prefix = `https://${ETHERSCAN_PREFIXES[
+    chainId as keyof typeof ETHERSCAN_PREFIXES
+  ] || ETHERSCAN_PREFIXES[1]}etherscan.io`;
+
+  switch (type) {
+    case 'transaction':
+      return `${prefix}/tx/${data}`;
+    case 'address':
+    default:
+      return `${prefix}/address/${data}`;
+  }
+};
+
+export const truncateAddress = (address: string): string =>
+  `${address.slice(0, 6)}…${address.slice(-4)}`;
