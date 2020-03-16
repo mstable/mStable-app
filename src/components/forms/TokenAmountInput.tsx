b195addfe07627a928aa6eb6343076135b33c18b
James Lefrère <james@colony.io>
2020-03-16 17:21:40 +0100

    Support token forms
    * Add a basic mUSD savings form for deposit/withdrawal
    * Add components to select amounts and tokens
    * Fetch balances using the tokens provider/GraphQL and format the balances

---
+++ src/components/forms/TokenAmountInput.tsx
@@ -0,0 +1,57 @@
+import React, { FC, useMemo } from 'react';
+import { AmountInput } from './AmountInput';
+import { TokenInput } from './TokenInput';
+import { useErc20TokensQuery } from '../../graphql/generated';
+
+interface Props {
+  error?: string;
+  tokenValue: string | null;
+  amountValue: string | void;
+  tokenAddresses: string[];
+  onChangeAmount?(amount: string): void;
+  onChangeToken?(token: string): void;
+}
+
+/**
+ * TokenAmountInput
+ * Select a token and an amount denominated in that token.
+ *
+ * @param error Error message, e.g. 'Amount too low'
+ * @param tokenAddresses List of available token addresses
+ * @param tokenValue Selected token address
+ * @param amountValue Chosen amount, e.g. '0.12'
+ * @param onChangeAmount Optional callback with the amount value
+ * @param onChangeToken Optional callback with the selected token
+ */
+export const TokenAmountInput: FC<Props> = ({
+  error,
+  tokenAddresses,
+  tokenValue,
+  amountValue,
+  onChangeAmount,
+  onChangeToken,
+}) => {
+  const { data: tokensData } = useErc20TokensQuery({
+    variables: { addresses: tokenAddresses },
+  });
+  const selectedToken = useMemo(
+    () => tokensData?.tokens.find(t => t.address === tokenValue),
+    [tokensData, tokenValue],
+  );
+  return (
+    <>
+      <AmountInput
+        error={error}
+        value={amountValue}
+        onChange={onChangeAmount}
+        decimals={selectedToken?.decimals}
+      />
+      <TokenInput
+        value={tokenValue}
+        tokens={tokensData?.tokens || []}
+        onChange={onChangeToken}
+      />
+      <div>{error}</div>
+    </>
+  );
+};
