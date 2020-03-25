7943c8be9ef4da3a096bc377d71089807b6acf77
James Lefrère <james@colony.io>
2020-03-25 11:12:23 +0100

    Add Activity placeholder
    * Add an activity placeholder by the wallet button

--- src/components/activity/Activity.tsx
+++ src/components/activity/Activity.tsx
@@ -1,7 +1,25 @@
 import React, { FC } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Container = styled.div`
+  background: ${props => props.theme.color.blue};
+  width: 16px;
+  height: 16px;
+  animation: ${rotate} 1s linear infinite;
+`;
 
 /**
  * Component to show app activity such as pending transactions.
  * @TODO design and content
  */
-export const Activity: FC<{}> = () => <></>;
+export const Activity: FC<{}> = () => <Container />;
