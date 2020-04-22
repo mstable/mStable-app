0ed77e88d6a3d695dbfcfb8d9abb9e1dac69fc10
James Lefrère <james@colony.io>
2020-04-22 16:25:02 +0200

    Add mUSD icon
    * Add '$' mUSD icon
    * Add 'm$' icon (commented out)

---
+++ src/components/core/MUSDIcon.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { ReactComponent as IconSVG } from './mUSD.svg';
+// import { ReactComponent as IconSVG } from './mUSD-m.svg';
+
+export const MUSDIcon = styled(IconSVG)`
+  position: relative;
+`;
