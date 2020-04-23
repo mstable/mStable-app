f5ff42b533d093a046afc6e1d81c2e94745ca9c9
James Lefrère <james@lefrere.com>
2020-04-23 17:11:26 +0200

    Fix token updating hooks (#31)
    * Adjust token updating hooks (balance and allowance) so that the values are refreshed when the account changes
    * Add `useAsyncMutex` hook to wait for the updating to finish before re-running
    * Use a deep comparison for the subscribed tokens array; should still be performant

--- src/web3/hooks.ts
+++ src/web3/hooks.ts
@@ -1,5 +1,6 @@
-import { useMemo, createElement, DOMElement } from 'react';
+import { useMemo, createElement, DOMElement, useEffect } from 'react';
 import blockies from 'ethereum-blockies';
+import { useMutex } from 'react-context-mutex';
 import { MassetNames } from '../types';
 import { TokenDetailsFragment, useCoreTokensQuery } from '../graphql/generated';
 import { truncateAddress } from './strings';
@@ -39,3 +40,34 @@ export const useBlockie = (
       },
     });
   }, [address]);
+
+/**
+ * Given a unique key and a promise, run the promise within a mutex
+ * (via `react-context-mutex`) and unlock the mutex when the promise
+ * resolves or rejects.
+ *
+ * @param mutexKey Unique key to lock the mutex
+ * @param callback Async callback to await when the mutex is run
+ */
+export const useAsyncMutex = (
+  mutexKey: string,
+  callback: () => Promise<void>,
+): void => {
+  const MutexRunner = useMutex();
+
+  useEffect(() => {
+    const mutex = new MutexRunner(mutexKey);
+
+    mutex.run(async () => {
+      mutex.lock();
+      try {
+        await callback();
+        mutex.unlock();
+      } catch (error) {
+        mutex.unlock();
+        throw error;
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mutexKey]);
+};
