3daeec03b146329ff5db6d958a3036369e2d4957
James Lefrère <james@colony.io>
2020-07-30 12:12:53 +0200

    Remove workaround for Graph block filter issue

--- src/context/earn/useRawEarnData.ts
+++ src/context/earn/useRawEarnData.ts
@@ -1,40 +1,11 @@
-import { useMemo } from 'react';
-import { gql } from '@apollo/client/core';
-import { useQuery } from '@apollo/react-hooks';
-
 import { useWallet } from 'use-wallet';
+
 import { useBlockPollingSubscription } from '../DataProvider/subscriptions';
 import {
-  RewardsPerTokenStoredAtBlockQueryResult,
+  useRewardsPerTokenStoredAtBlockQuery,
   useStakingRewardsContractsLazyQuery,
 } from '../../graphql/mstable';
 import { RawEarnData, SyncedEarnData } from './types';
-import { BlockTimestamp } from '../../types';
-
-const useRewardsPerTokenStoredAtBlockQuery = (
-  blockTimestamp?: BlockTimestamp,
-): RewardsPerTokenStoredAtBlockQueryResult => {
-  // Can't query blocks with variables yet:
-  // https://github.com/graphprotocol/graph-node/issues/1460
-  const query = useMemo(() => {
-    const queryString = `
-      query RewardsPerTokenStoredAtBlockQueryResult
-      @api(name: mstable) {
-          stakingRewardsContracts(block: { number: ${blockTimestamp?.blockNumber} }) {
-              id
-              rewardPerTokenStored
-              platformRewardPerTokenStored
-          }
-      }
-  `;
-    return gql(queryString);
-  }, [blockTimestamp]);
-
-  return useQuery(query, {
-    skip: !blockTimestamp,
-    fetchPolicy: 'cache-and-network',
-  }) as RewardsPerTokenStoredAtBlockQueryResult;
-};
 
 export const useRawEarnData = ({
   block24hAgo,
@@ -49,7 +20,11 @@ export const useRawEarnData = ({
     stakingRewardsContractsSub.data?.stakingRewardsContracts || [];
 
   const rawStakingRewardsContracts24hAgoQuery = useRewardsPerTokenStoredAtBlockQuery(
-    block24hAgo,
+    {
+      variables: { blockNumber: block24hAgo?.blockNumber as number },
+      skip: !block24hAgo,
+      fetchPolicy: 'cache-first', // Safe to use because the block number will change
+    },
   );
   const rawStakingRewardsContracts24hAgo =
     rawStakingRewardsContracts24hAgoQuery.data?.stakingRewardsContracts || [];
