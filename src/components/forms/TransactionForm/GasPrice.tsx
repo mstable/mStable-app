85bc0567b870c02f4527fda0b4062c5e695ffdf5
lovrobiljeskovic <biljeskovic.lovro@gmail.com>
2020-11-12 12:24:50 +0100

    Addec custom gas fee calculation

--- src/components/forms/TransactionForm/GasPrice.tsx
+++ src/components/forms/TransactionForm/GasPrice.tsx
@@ -142,10 +142,17 @@ export const GasPrice: FC<Props> = () => {
   const setGasPriceType = useSetGasPriceType();
   const currentGasPriceType = useCurrentGasPriceType();
   const currentGasPrice = useCurrentGasPrice();
+  const customTransactionFee =
+    currentGasPriceType === GasPriceType.Custom &&
+    currentGasPrice &&
+    ethPrice &&
+    manifest?.gasLimit
+      ? currentGasPrice * (ethPrice / 1e9) * manifest.gasLimit.toNumber()
+      : undefined;
 
   const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
     event => {
-      setGasPrice(parseInt(event.target.value, 10));
+      setGasPrice(parseFloat(event.target.value));
     },
     [setGasPrice],
   );
@@ -160,12 +167,7 @@ export const GasPrice: FC<Props> = () => {
   return (
     <Container>
       {gasPriceOptions.map(({ type, label, name }) => {
-        const gasPrice =
-          type === GasPriceType.Custom
-            ? currentGasPrice
-            : name && gasPrices
-            ? gasPrices[name]
-            : undefined;
+        const gasPrice = name && gasPrices ? gasPrices[name] : undefined;
         const transactionFee =
           gasPrice && ethPrice && manifest?.gasLimit
             ? gasPrice * (ethPrice / 1e9) * manifest.gasLimit.toNumber()
@@ -195,7 +197,9 @@ export const GasPrice: FC<Props> = () => {
                     onChange={handleChange}
                   />
                   <p>
-                    {transactionFee ? `$ ${transactionFee.toFixed(2)}` : '$ –'}
+                    {customTransactionFee
+                      ? `$ ${customTransactionFee.toFixed(2)}`
+                      : '$ –'}
                   </p>
                 </FlexContainer>
               )}
