057b14bee719399f5e2d36a748c9a31d459f107b
chrisjgf <chrisjgf@gmail.com>
2020-11-30 17:10:23 +0100

    Change colors to theme colors

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,35 +1,21 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC } from 'react';
 import styled from 'styled-components';
-import { useHistory } from 'react-router-dom';
 import { ReactComponent as MstableIcon } from '../../icons/mstable_horizontal.svg';
 import { ReactComponent as MusdIcon } from '../../icons/musd_logo.svg';
 import { ReactComponent as BtcIcon } from '../../icons/btc_logo.svg';
 import { useSetSelectedMasset } from '../../../context/MassetsProvider';
 import { UnstyledButton } from '../../core/Button';
-import { ViewportWidth } from '../../../theme';
 
 const Container = styled.div`
   display: flex;
+  height: 100%;
+  width: 100%;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  :first-child {
-    margin-bottom: 1.5rem;
-  }
 `;
 
-const MassetsContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-flow: wrap;
-  max-width: 100%;
-  @media (min-width: ${ViewportWidth.m}) {
-    flex-wrap: initial;
-  }
-`;
-
-const MusdIconContainer = styled.div`
-  background: #176ede;
+const IconContainer = styled.div`
   border: 4px solid rgba(255, 255, 255, 0.25);
   border-radius: 50%;
   justify-content: center;
@@ -44,8 +30,10 @@ const MusdIconContainer = styled.div`
   }
 `;
 
-const MbtcIconContainer = styled(MusdIconContainer)`
-  background: #ffa825;
+const MassetsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
 `;
 
 const Musd = styled(UnstyledButton)`
@@ -55,7 +43,7 @@ const Musd = styled(UnstyledButton)`
   justify-content: center;
   flex-direction: column;
   align-items: center;
-  background: #176ede;
+  background: ${({ theme }) => theme.color.blue};
   width: 18rem;
   height: 14rem;
   border-radius: 32px;
@@ -67,33 +55,30 @@ const Musd = styled(UnstyledButton)`
     font-size: 32px;
     color: #ffffff;
   }
+  border: 10px solid #e3efff;
 `;
 
 const Mbtc = styled(Musd)`
-  background: #ffa825;
+  background: ${({ theme }) => theme.color.gold};
+  border: 10px solid #ffedd1;
 `;
 
 export const Home: FC = () => {
   const selectMasset = useSetSelectedMasset();
-  const history = useHistory();
-  const handleMusdClick = useCallback(() => {
-    selectMasset('mUSD');
-    history.push('/musd/mint');
-  }, [history, selectMasset]);
   return (
     <Container>
       <MstableIcon />
       <MassetsContainer>
-        <Musd onClick={handleMusdClick}>
-          <MusdIconContainer>
+        <Musd onClick={() => selectMasset('mUSD')}>
+          <IconContainer>
             <MusdIcon />
-          </MusdIconContainer>
+          </IconContainer>
           <span>mUSD</span>
         </Musd>
         <Mbtc onClick={() => selectMasset('mUSD')}>
-          <MbtcIconContainer>
+          <IconContainer>
             <BtcIcon />
-          </MbtcIconContainer>
+          </IconContainer>
           <span>mBTC</span>
         </Mbtc>
       </MassetsContainer>
