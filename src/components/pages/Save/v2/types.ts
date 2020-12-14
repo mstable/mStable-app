36cefd75facbb51865169c2973ac10a119656d2d
James Lefrère <james@colony.io>
2020-12-14 17:59:51 +0100

    Copy v1 Save files
    - Copy `v1` Save page files (components, providers, etc) to `v2` so that `v1` files can be easily deprecated
    - Remove references to v1 from v2 and vice-versa
    - Reorganise `Save` component to be more flexible re: versions

---
+++ src/components/pages/Save/v2/types.ts
@@ -0,0 +1,58 @@
+import { MassetState } from '../../../../context/DataProvider/types';
+import { BigDecimal } from '../../../../web3/BigDecimal';
+
+export enum TransactionType {
+  Deposit,
+  Withdraw,
+}
+
+export enum Reasons {
+  AmountMustBeGreaterThanZero = 'Amount must be greater than zero',
+  AmountMustBeSet = 'Amount must be set',
+  DepositAmountMustNotExceedTokenBalance = 'Deposit amount must not exceed token balance',
+  FetchingData = 'Fetching data',
+  MUSDMustBeApproved = 'Transfers must be approved',
+  TokenMustBeSelected = 'Token must be selected',
+  WithdrawAmountMustNotExceedSavingsBalance = 'Withdraw amount must not exceed savings balance',
+}
+
+export enum Actions {
+  Data,
+  SetAmount,
+  SetMaxAmount,
+  ToggleTransactionType,
+}
+
+export interface State {
+  error?: string;
+  transactionType: TransactionType;
+  amount?: BigDecimal;
+  amountInCredits?: BigDecimal;
+  formValue: string | null;
+  touched: boolean;
+  valid: boolean;
+  initialized: boolean;
+  simulated?: MassetState;
+  massetState?: MassetState;
+  needsUnlock?: boolean;
+}
+
+export type Action =
+  | {
+      type: Actions.Data;
+      payload?: MassetState;
+    }
+  | {
+      type: Actions.SetAmount;
+      payload: {
+        formValue: string | null;
+      };
+    }
+  | { type: Actions.SetMaxAmount }
+  | { type: Actions.ToggleTransactionType };
+
+export interface Dispatch {
+  setAmount(formValue: string | null, isCreditAmount?: boolean): void;
+  setMaxAmount(): void;
+  toggleTransactionType(): void;
+}
