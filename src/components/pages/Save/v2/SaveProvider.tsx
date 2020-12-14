5a2a0d6e2cb71cacaadccf0107cd5fbb5b6c7b9d
James Lefrère <james@colony.io>
2020-12-14 17:59:51 +0100

    Copy v1 Save files
    - Copy `v1` Save page files (components, providers, etc) to `v2` so that `v1` files can be easily deprecated
    - Remove references to v1 from v2 and vice-versa
    - Reorganise `Save` component to be more flexible re: versions

---
+++ src/components/pages/Save/v2/SaveProvider.tsx
@@ -0,0 +1,70 @@
+import React, {
+  createContext,
+  FC,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useReducer,
+} from 'react';
+
+import { useSelectedMassetState } from '../../../../context/DataProvider/DataProvider';
+import { reducer } from './reducer';
+import { Actions, Dispatch, State, TransactionType } from './types';
+
+const initialState: State = {
+  formValue: null,
+  initialized: false,
+  touched: false,
+  transactionType: TransactionType.Deposit,
+  valid: false,
+};
+
+const stateCtx = createContext<State>(initialState);
+const dispatchCtx = createContext<Dispatch>({} as Dispatch);
+
+export const SaveProvider: FC = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const massetState = useSelectedMassetState();
+
+  useEffect(() => {
+    dispatch({ type: Actions.Data, payload: massetState });
+  }, [massetState]);
+
+  const setAmount = useCallback<Dispatch['setAmount']>(
+    formValue => {
+      dispatch({
+        type: Actions.SetAmount,
+        payload: { formValue },
+      });
+    },
+    [dispatch],
+  );
+
+  const setMaxAmount = useCallback<Dispatch['setMaxAmount']>(() => {
+    dispatch({ type: Actions.SetMaxAmount });
+  }, [dispatch]);
+
+  const toggleTransactionType = useCallback<
+    Dispatch['toggleTransactionType']
+  >(() => {
+    dispatch({ type: Actions.ToggleTransactionType });
+  }, [dispatch]);
+
+  return (
+    <stateCtx.Provider value={state}>
+      <dispatchCtx.Provider
+        value={useMemo(
+          () => ({ setAmount, setMaxAmount, toggleTransactionType }),
+          [setMaxAmount, setAmount, toggleTransactionType],
+        )}
+      >
+        {children}
+      </dispatchCtx.Provider>
+    </stateCtx.Provider>
+  );
+};
+
+export const useSaveState = (): State => useContext(stateCtx);
+
+export const useSaveDispatch = (): Dispatch => useContext(dispatchCtx);
