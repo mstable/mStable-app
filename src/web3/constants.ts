5a2a880897013a296230d74faa210e5e749e57a1
lovrobiljeskovic <biljeskovic.lovro@gmail.com>
2020-12-17 14:10:35 +0100

    Updated typings in constants Use `ADDRESSES` in the `TokenProvider`

--- src/web3/constants.ts
+++ src/web3/constants.ts
@@ -17,27 +17,41 @@ export const NETWORK_NAMES = {
   1337: 'Local network',
 };
 
-interface Network {
-  [key: number]: object;
+interface Addresses {
+  MTA: string;
+
+  mUSD: {
+    SaveWrapper: string;
+  };
+  mBTC?: {
+    SaveWrapper: string;
+  };
 }
 
-const ADDRESSES_BY_NETWORK: Network = Object.freeze({
+type AddressesByNetwork = Record<typeof CHAIN_ID, Addresses>;
+
+const ADDRESSES_BY_NETWORK: AddressesByNetwork = Object.freeze({
   1: {
     MTA: '0xa3bed4e1c75d00fa6f4e5e6922db7261b5e9acd2',
-    SaveWrapper: 'TODO',
+    mUSD: {
+      SaveWrapper: 'TODO',
+    },
   },
   3: {
     MTA: '0x273bc479e5c21caa15aa8538decbf310981d14c0',
-    SaveWrapper: '0xF078E3146160459fF7bb09D1D5e2D40238A7bCbe',
+    mUSD: {
+      SaveWrapper: '0xF078E3146160459fF7bb09D1D5e2D40238A7bCbe',
+    },
   },
   42: {
     MTA: '0xcda64b5d3ca85800ab9f7409686985b59f2b9598',
-    SaveWrapper: 'TODO',
+    mUSD: {
+      SaveWrapper: 'TODO',
+    },
   },
 });
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const ADDRESSES = ADDRESSES_BY_NETWORK[CHAIN_ID];
+export const ADDRESSES: Addresses = ADDRESSES_BY_NETWORK[CHAIN_ID];
 
 export const EMOJIS = {
   error: '❌',
