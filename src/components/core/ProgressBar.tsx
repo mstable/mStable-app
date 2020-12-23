6e4f0b6a9f3e853a6945bf622de3ce28adf20c02
James Lefrere <james.lefrere@mstable.org>
2020-12-23 18:02:34 +0100

    Make progress bar fancier

--- src/components/core/ProgressBar.tsx
+++ src/components/core/ProgressBar.tsx
@@ -1,40 +1,87 @@
 import React, { FC } from 'react';
-import Styled from 'styled-components';
+import styled from 'styled-components';
 
 interface Props {
   value?: number;
   max?: number;
+  min?: number;
   color?: string;
+  highlight?: string;
 }
 
-const Container = Styled.div<{ color?: string }>`
-  progress {
-    margin-right: 8px;
-  }
+const HEIGHT = 5;
+const WIDTH = 100;
 
-  progress[value] {
-    width: 100%;
-    -webkit-appearance: none;
-    appearance: none;
-  }
-
-  progress[value]::-webkit-progress-bar {
-    height: 10px;
-    border-radius: 20px;
-    background-color: #eee;
-  }  
-
-  progress[value]::-webkit-progress-value {
-    height: 10px;
-    border-radius: 20px;
-    background-color: ${props => props.color};
+const Container = styled.svg`
+  g > rect {
+    transition: width 0.5s ease-in-out;
   }
 `;
 
-export const ProgressBar: FC<Props> = ({ value, max, color }) => {
+export const ProgressBar: FC<Props> = ({
+  value = 0,
+  max = 1,
+  min = 0,
+  color = '#67C73A',
+  highlight = '#77D16F',
+}) => {
   return (
-    <Container color={color}>
-      <progress value={value} max={max} />
+    <Container
+      viewBox={`0 0 ${WIDTH} ${HEIGHT}`}
+      preserveAspectRatio="none"
+      color={color}
+    >
+      <pattern
+        id="hatch"
+        patternUnits="userSpaceOnUse"
+        patternTransform="rotate(45 0 0)"
+        width="4"
+        height="4"
+      >
+        <rect x={0} y={0} width={4} height={4} fill={color} />
+        <animateTransform
+          attributeType="xml"
+          attributeName="patternTransform"
+          type="translateX"
+          from="4"
+          to="0"
+          begin="0"
+          dur="0.5s"
+          repeatCount="indefinite"
+          additive="sum"
+        />
+        <line
+          x1="0"
+          y1="0"
+          x2="0"
+          y2="4"
+          style={{
+            stroke: highlight,
+            strokeWidth: 4,
+          }}
+        />
+      </pattern>
+      <g>
+        <rect
+          width={WIDTH}
+          height={HEIGHT}
+          x={0}
+          y={0}
+          rx={HEIGHT / 2}
+          ry={HEIGHT / 2}
+          fill="#f8f8f8"
+          stroke="#eee"
+        />
+        <rect
+          width={Math.max(((value - min) / (max - min)) * WIDTH, HEIGHT)}
+          height={HEIGHT}
+          x={0}
+          y={0}
+          rx={HEIGHT / 2}
+          ry={HEIGHT / 2}
+          fill="url(#hatch)"
+        />
+      </g>
     </Container>
   );
 };
