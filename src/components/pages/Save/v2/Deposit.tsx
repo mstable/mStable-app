b12370e4353f7eb18948b6b02dc2744c630da110
chrisjgf <chrisjgf@gmail.com>
2021-01-08 15:51:33 +0000

    Move Boost higher in view & make conditional

--- src/components/pages/Save/v2/Deposit.tsx
+++ src/components/pages/Save/v2/Deposit.tsx
@@ -2,13 +2,11 @@ import React, { FC } from 'react';
 import styled from 'styled-components';
 
 import { AssetExchange } from './AssetExchange';
-import { Boost } from './Boost';
 
 const Container = styled.div``;
 
 export const Deposit: FC = () => (
   <Container>
-    <Boost />
     <AssetExchange />
   </Container>
 );
