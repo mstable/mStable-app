6e29037015d0d95a1cfa40f7a7727df55f3d1608
James Lefrere <james.lefrere@mstable.org>
2021-01-11 18:12:56 +0100

    Remove unused functionality
    - Remove unused account/layout functionality

--- src/components/layout/Account.tsx
+++ src/components/layout/Account.tsx
@@ -1,10 +1,9 @@
 import React, { FC } from 'react';
 
-import { AccountItems, useAccountItem } from '../../context/AppProvider';
+import { useAccountOpen } from '../../context/AppProvider';
 import { Wallet } from '../wallet/Wallet';
 
 export const Account: FC = () => {
-  const accountItem = useAccountItem();
-
-  return accountItem === AccountItems.Wallet ? <Wallet /> : null;
+  const accountOpen = useAccountOpen();
+  return accountOpen ? <Wallet /> : null;
 };
