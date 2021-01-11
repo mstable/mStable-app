b192ac378709f59208741b93dc9009363b203514
James Lefrere <james.lefrere@mstable.org>
2021-01-11 18:12:27 +0100

    Add modals
    - Add modals (with example that can later be removed)

--- src/components/pages/Save/v2/Deposit.tsx
+++ src/components/pages/Save/v2/Deposit.tsx
@@ -1,12 +1,23 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 
+import { useModalComponent } from '../../../../hooks/useModalComponent';
+import { Button } from '../../../core/Button';
 import { AssetExchange } from './AssetExchange';
 
 const Container = styled.div``;
 
-export const Deposit: FC = () => (
-  <Container>
-    <AssetExchange />
-  </Container>
-);
+export const Deposit: FC = () => {
+  // TODO replace me
+  const [showModal] = useModalComponent({
+    title: 'Modal example',
+    children: <div>Modal children</div>,
+  });
+
+  return (
+    <Container>
+      <Button onClick={showModal}>Test modal</Button>
+      <AssetExchange />
+    </Container>
+  );
+};
