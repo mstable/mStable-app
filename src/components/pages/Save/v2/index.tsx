eef87c18ca67a3ff7f946f383404c25b27803323
James Lefrere <james.lefrere@mstable.org>
2021-01-12 10:41:35 +0100

    Add save modal placeholders

--- src/components/pages/Save/v2/index.tsx
+++ src/components/pages/Save/v2/index.tsx
@@ -1,21 +1,8 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 
-import { useSaveState, useSaveDispatch, SaveProvider } from './SaveProvider';
-import { TabsContainer, TabBtn } from '../../../core/Tabs';
-import { Deposit } from './Deposit';
-import { Withdraw } from './Withdraw';
-import { SaveMode } from './types';
-import { Boost } from './Boost';
-
-const MODE_TYPES = {
-  [SaveMode.Deposit]: {
-    label: 'Deposit',
-  },
-  [SaveMode.Withdraw]: {
-    label: 'Withdraw',
-  },
-};
+import { Button } from '../../../core/Button';
+import { useModalComponent } from '../../../../hooks/useModalComponent';
 
 const Container = styled.div`
   display: flex;
@@ -23,44 +10,105 @@ const Container = styled.div`
   gap: 2rem;
   border-radius: 0 0 2px 2px;
   text-align: left;
+
+  > * {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 1rem;
+    border: 1px #eee solid;
+    padding: 1rem;
+    > :last-child {
+      display: flex;
+      gap: 1rem;
+    }
+  }
 `;
 
-const TabButton: FC<{ tabMode: SaveMode }> = ({ tabMode }) => {
-  const { mode } = useSaveState();
-  const { setModeType } = useSaveDispatch();
-  return (
-    <TabBtn
-      type="button"
-      onClick={() => setModeType(tabMode)}
-      active={mode === tabMode}
-    >
-      {MODE_TYPES[tabMode].label}
-    </TabBtn>
-  );
+const SaveModal: FC = () => {
+  return <div>SaveModal</div>;
 };
 
-const SaveForm: FC = () => {
-  const { mode } = useSaveState();
+const SaveAndStakeModal: FC = () => {
+  return <div>SaveAndStakeModal</div>;
+};
 
-  // TODO: - Make boost conditionally render if use has staked balance
-  const userHasStaked = false;
+const SaveDepositModal: FC = () => {
+  return <div>SaveDepositModal</div>;
+};
 
-  return (
-    <Container>
-      {userHasStaked && <Boost />}
-      <TabsContainer>
-        <TabButton tabMode={SaveMode.Deposit} />
-        <TabButton tabMode={SaveMode.Withdraw} />
-      </TabsContainer>
-      {mode === SaveMode.Deposit ? <Deposit /> : <Withdraw />}
-    </Container>
-  );
+const SaveWithdrawModal: FC = () => {
+  return <div>SaveWithdrawModal</div>;
+};
+
+const SaveStakeModal: FC = () => {
+  return <div>SaveStakeModal</div>;
+};
+
+const VaultWithdrawModal: FC = () => {
+  return <div>VaultWithdrawModal</div>;
+};
+
+const VaultExitModal: FC = () => {
+  return <div>VaultExitModal</div>;
 };
 
 export const Save: FC = () => {
+  const [showSaveModal] = useModalComponent({
+    title: 'Save mUSD',
+    children: <SaveModal />,
+  });
+  const [showSaveAndStakeModal] = useModalComponent({
+    title: 'Save and Stake mUSD',
+    children: <SaveAndStakeModal />,
+  });
+
+  const [showSaveDepositModal] = useModalComponent({
+    title: 'Deposit assets for imUSD',
+    children: <SaveDepositModal />,
+  });
+  const [showSaveWithdrawModal] = useModalComponent({
+    title: 'Withdraw imUSD',
+    children: <SaveWithdrawModal />,
+  });
+  const [showSaveStakeModal] = useModalComponent({
+    title: 'Stake imUSD',
+    children: <SaveStakeModal />,
+  });
+
+  const [showVaultWithdrawModal] = useModalComponent({
+    title: 'Withdraw Stake',
+    children: <VaultWithdrawModal />,
+  });
+  const [showVaultExitModal] = useModalComponent({
+    title: 'Exit Vault',
+    children: <VaultExitModal />,
+  });
+
   return (
-    <SaveProvider>
-      <SaveForm />
-    </SaveProvider>
+    <Container>
+      <div>
+        <div>mUSD</div>
+        <div>
+          <Button onClick={showSaveModal}>Save</Button>
+          <Button onClick={showSaveAndStakeModal}>Save & Stake</Button>
+        </div>
+      </div>
+      <div>
+        <div>imUSD</div>
+        <div>
+          <Button onClick={showSaveDepositModal}>Deposit</Button>
+          <Button onClick={showSaveWithdrawModal}>Withdraw</Button>
+          <Button onClick={showSaveStakeModal}>Stake</Button>
+        </div>
+      </div>
+      <div>
+        <div>imUSD Vault</div>
+        <div>
+          <Button onClick={showVaultWithdrawModal}>Withdraw</Button>
+          <Button onClick={showVaultExitModal}>Exit</Button>
+        </div>
+      </div>
+    </Container>
   );
 };
