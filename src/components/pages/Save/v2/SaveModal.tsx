67f329dd6ec08c868687fe25b5f628b1262bb267
James Lefrere <james.lefrere@mstable.org>
2021-01-18 13:40:16 +0100

    Show withdraw when not active

--- src/components/pages/Save/v2/SaveModal.tsx
+++ src/components/pages/Save/v2/SaveModal.tsx
@@ -21,53 +21,49 @@ const Container = styled.div`
   }
 `;
 
-const TABS: { tab: Tabs; label: string; component: FC }[] = [
-  {
-    tab: Tabs.DepositStablecoins,
-    label: 'Deposit stablecoins',
-    component: SaveDeposit,
-  },
-  ...(ADDRESSES.mUSD.SaveWrapper
-    ? [
-        {
-          tab: Tabs.DepositETH,
-          label: 'Deposit ETH',
-          component: SaveDepositETH,
-        },
-      ]
-    : []),
-  {
-    tab: Tabs.Redeem,
-    label: 'Redeem collateral',
-    component: SaveRedeem,
-  },
-];
-
 // TODO TabbedModal
 export const SaveModal: FC = () => {
   const massetState = useSelectedMassetState();
   const isActive = massetState?.savingsContracts.v2?.active;
-  const [tab, setTab] = useState<Tabs>(Tabs.DepositStablecoins);
-  const activeTab = useMemo(() => TABS.find(_tab => _tab.tab === tab), [tab]);
 
-  if (!isActive) {
-    return (
-      <Container>
-        <TabsContainer>
-          <TabBtn active>Pre-deposit</TabBtn>
-        </TabsContainer>
-        <PreDeposit />
-      </Container>
-    );
-  }
+  const [tab, setTab] = useState<Tabs>(Tabs.DepositStablecoins);
+
+  const [tabs, ActiveComponent] = useMemo(() => {
+    const _tabs = [
+      {
+        tab: Tabs.DepositStablecoins,
+        label: isActive ? 'Deposit stablecoins' : 'Pre-deposit',
+        component: isActive ? SaveDeposit : PreDeposit,
+        active: tab === Tabs.DepositStablecoins,
+      },
+      ...(isActive && ADDRESSES.mUSD.SaveWrapper
+        ? [
+            {
+              tab: Tabs.DepositETH,
+              label: 'Deposit ETH',
+              component: SaveDepositETH,
+              active: tab === Tabs.DepositETH,
+            },
+          ]
+        : []),
+      {
+        tab: Tabs.Redeem,
+        label: 'Redeem collateral',
+        component: SaveRedeem,
+        active: tab === Tabs.Redeem,
+      },
+    ];
+    const activeComponent = _tabs.find(t => t.active)?.component as FC;
+    return [_tabs, activeComponent];
+  }, [tab, isActive]);
 
   return (
     <Container>
       <TabsContainer>
-        {TABS.map(_tab => (
+        {tabs.map(_tab => (
           <TabBtn
             key={_tab.tab.toString()}
-            active={_tab.tab === tab}
+            active={_tab.active}
             onClick={() => {
               setTab(_tab.tab);
             }}
@@ -76,7 +72,7 @@ export const SaveModal: FC = () => {
           </TabBtn>
         ))}
       </TabsContainer>
-      {activeTab?.component && <activeTab.component />}
+      {ActiveComponent && <ActiveComponent />}
     </Container>
   );
 };
