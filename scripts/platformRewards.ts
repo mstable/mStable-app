4a8e81423d9a97905f674aa449027fa16452dda1
James Lefrere <james.lefrere@mstable.org>
2021-01-27 10:33:59 +0100

    BAL rewards tranche 20

--- scripts/platformRewards.ts
+++ scripts/platformRewards.ts
@@ -11,13 +11,13 @@
  * @example
  *
  * ```bash
-yarn run platform-rewards --trancheNumber=19 \
-  --startBlock=11633634 \
-  --startTimestamp=1610368270 \
+yarn run platform-rewards --trancheNumber=20 \
+  --startBlock=11679261 \
+  --startTimestamp=1610973086 \
   --token=0xba100000625a3754423978a60c9317c58a424e3d \
   --allocations \
-  0x881c72d1e6317f10a1cdcbe05040e7564e790c80,167.515578505310173796 \
-  0xf7575d4d4db78f6ba43c734616c51e9fd4baa7fb,920.914447095117338904
+  0x881c72d1e6317f10a1cdcbe05040e7564e790c80,163.507351478466603112 \
+  0xf7575d4d4db78f6ba43c734616c51e9fd4baa7fb,1117.009572434813208019
  * ```
  *
  *
