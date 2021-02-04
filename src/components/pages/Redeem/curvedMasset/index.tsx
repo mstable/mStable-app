c735e9c3efa8e652c4959e013f11814bdb3e1347
chrisjgf <chrisjgf@gmail.com>
2021-02-04 16:48:31 +0000

    Change tab + add dynamic descriptions

--- src/components/pages/Redeem/curvedMasset/index.tsx
+++ src/components/pages/Redeem/curvedMasset/index.tsx
@@ -2,26 +2,34 @@ import React, { FC, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
 import { useSelectedMassetState } from '../../../../context/DataProvider/DataProvider';
-
 import { PageAction, PageHeader } from '../../PageHeader';
-
 import { RedeemExactBassets } from './RedeemExactBassets';
 import { RedeemMasset } from './RedeemMasset';
 import { Toggle } from '../../../core/Toggle';
 
 enum ToggleOption {
-  Single = 'Single Asset',
-  Multiple = 'Multiple Assets',
+  Single = 'Single',
+  Multiple = 'Multiple',
 }
+
+const { Single, Multiple } = ToggleOption;
+
+const title: { [key in ToggleOption]: string } = {
+  [Single]: 'mBTC Amount',
+  [Multiple]: 'Underlying Amount',
+};
+
+const info: { [key in ToggleOption]: string } = {
+  [Single]: 'Redeem an exact amount of mBTC for its underlying collateral',
+  [Multiple]: 'Redeem mBTC for an exact amount of its underlying collateral',
+};
 
 export const Redeem: FC = () => {
   const massetState = useSelectedMassetState();
-  const [currentTab, setSelectedTab] = useState(ToggleOption.Single);
+  const [currentTab, setSelectedTab] = useState(Single);
 
-  const options = [ToggleOption.Single, ToggleOption.Multiple];
-
-  const toggleOptions = options.map(tab => ({
-    title: tab,
+  const toggleOptions = Object.keys(title).map(tab => ({
+    title: title[tab as ToggleOption],
     onClick: () => setSelectedTab(tab as ToggleOption),
     active: currentTab === tab,
   }));
@@ -30,10 +38,7 @@ export const Redeem: FC = () => {
 
   return massetState ? (
     <div>
-      <PageHeader
-        action={PageAction.Redeem}
-        subtitle={`Exchange ${massetState.token.symbol} for its underlying collateral`}
-      >
+      <PageHeader action={PageAction.Redeem} subtitle={info[currentTab]}>
         <Toggle options={toggleOptions} />
       </PageHeader>
       {isExactBassets ? <RedeemExactBassets /> : <RedeemMasset />}
