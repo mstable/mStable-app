f214d16f8ed2e8e6503c81ce0885464030c29421
chrisjgf <chrisjgf@gmail.com>
2021-02-10 16:38:19 +0000

    Start stubbing out provider

---
+++ src/components/pages/Mint/ren/types.ts
@@ -0,0 +1,38 @@
+import { MassetState } from '../../../../context/DataProvider/types';
+import { TransactionOption } from '../../../../types';
+
+export type OnboardData = {
+  inputAddress?: string;
+  inputFormValue?: string;
+  inputAddressOptions: TransactionOption[];
+  outputAddress?: string;
+  outputAddressOptions: TransactionOption[];
+};
+
+export enum Actions {
+  Data,
+  SetOnboardData,
+}
+
+export type Action =
+  | { type: Actions.Data; payload?: MassetState }
+  | {
+      type: Actions.SetOnboardData;
+      payload?: OnboardData;
+    };
+
+export interface State {
+  initialized: boolean;
+  step: number;
+  onboardData?: {
+    inputAddress?: string;
+    inputFormValue?: string;
+    inputAddressOptions: TransactionOption[];
+    outputAddress?: string;
+    outputAddressOptions: TransactionOption[];
+  };
+}
+
+export interface Dispatch {
+  setOnboardData(data?: OnboardData): void;
+}
