df68c4d426fec1555f02df59f4d7b79ff0c62284
James Lefrere <james.lefrere@mstable.org>
2021-03-30 14:12:28 +0200

    Add feeders to MassetStats

--- src/components/stats/SimpleMassetStats.tsx
+++ src/components/stats/SimpleMassetStats.tsx
@@ -3,10 +3,29 @@ import styled from 'styled-components';
 
 import { useSelectedMassetState } from '../../context/DataProvider/DataProvider';
 import { MassetState } from '../../context/DataProvider/types';
-import { Amount } from '../core/Amount';
+import { TokenIcon } from '../icons/TokenIcon';
+import { useSelectedMassetPrice } from '../../hooks/usePrice';
+import { CountUp, CountUpUSD } from '../core/CountUp';
+import { toK } from './utils';
+import { TokenPair } from '../pages/Pools/cards/AssetCard';
 
 const Label = styled.div`
   font-weight: 600;
+  display: flex;
+  align-items: center;
+
+  > :first-child {
+    height: 2rem;
+    width: auto;
+    margin-right: 0.5rem;
+  }
+`;
+
+const Percentage = styled.div`
+  text-align: right;
+  > :last-child {
+    ${({ theme }) => theme.mixins.numeric};
+  }
 `;
 
 const AssetRow = styled.div`
@@ -16,17 +35,21 @@ const AssetRow = styled.div`
   padding: 0.75rem 0;
   border-bottom: 1px ${({ theme }) => theme.color.bodyTransparent} solid;
 
+  > :last-child span {
+    text-align: right;
+  }
+
   &:last-of-type {
     margin-bottom: 0;
     border-bottom: 0;
     padding-bottom: 0;
   }
+`;
 
-  > :last-child {
-    text-align: right;
-    > :last-child {
-      ${({ theme }) => theme.mixins.numeric};
-    }
+const FeederRow = styled(AssetRow)`
+  display: block;
+  > :first-child {
+    margin-bottom: 0.5rem;
   }
 `;
 
@@ -40,18 +63,29 @@ const Container = styled.div`
 
 export const SimpleMassetStats: FC = () => {
   const masset = useSelectedMassetState() as MassetState;
+  const massetPrice = useSelectedMassetPrice();
 
-  return masset ? (
+  return (
     <Container>
       <AssetRow>
-        <Label>Total {masset.token.symbol}</Label>
-        <Amount amount={masset.token.totalSupply} />
+        <Label>
+          <TokenIcon symbol={masset.token.symbol} />
+          Total {masset.token.symbol}
+        </Label>
+        <CountUpUSD
+          end={masset.token.totalSupply.simple}
+          price={massetPrice}
+          formattingFn={toK}
+        />
       </AssetRow>
-      {Object.values(masset.bAssets).map((b) => (
+      {Object.values(masset.bAssets).map(b => (
         <AssetRow key={b.token.address}>
-          <Label>{b.token.symbol}</Label>
-          <div>
-            <Amount amount={b.totalVaultInMasset} />
+          <Label>
+            <TokenIcon symbol={b.token.symbol} />
+            {b.token.symbol}
+          </Label>
+          <Percentage>
+            <CountUp end={b.totalVaultInMasset.simple} formattingFn={toK} />
             <div>
               {b.totalVaultInMasset.simple > 0 &&
               masset.token.totalSupply.simple > 0
@@ -62,9 +96,27 @@ export const SimpleMassetStats: FC = () => {
                   ).toFixed(2)}%`
                 : null}
             </div>
-          </div>
+          </Percentage>
         </AssetRow>
       ))}
+      <FeederRow>
+        <Label>Feeder Pools</Label>
+        <div>
+          {Object.values(masset.feederPools).map(fp => (
+            <AssetRow key={fp.address}>
+              <Label>
+                <TokenPair tokens={[fp.masset.token, fp.fasset.token]} />
+                {fp.token.symbol}
+              </Label>
+              <CountUpUSD
+                end={fp.liquidity.simple}
+                price={massetPrice ? massetPrice * fp.price.simple : undefined}
+                formattingFn={toK}
+              />
+            </AssetRow>
+          ))}
+        </div>
+      </FeederRow>
     </Container>
-  ) : null;
+  );
 };
