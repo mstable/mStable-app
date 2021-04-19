215e2654450825ce87ac00f620b74c6c8c60c31b
chrisjgf <chrisjgf@gmail.com>
2021-04-19 18:12:22 +0100

    Refactor Dropdown

---
+++ src/components/core/NetworkDropdown.tsx
@@ -0,0 +1,40 @@
+import React, { FC, useState } from 'react';
+import styled from 'styled-components';
+import { Dropdown } from './Dropdown';
+
+enum Network {
+  Ethereum = 'Ethereum',
+  Polygon = 'Polygon',
+}
+
+const StyledDropdown = styled(Dropdown)`
+  * {
+    font-size: 1rem;
+  }
+`;
+
+const options = {
+  [Network.Ethereum]: {
+    symbol: 'ETH',
+  },
+  [Network.Polygon]: {},
+};
+
+export const NetworkDropdown: FC = () => {
+  const [selected, setSelected] = useState<string | undefined>(
+    Network.Ethereum,
+  );
+
+  const handleSelect = (selectedTitle?: string): void => {
+    if (!selectedTitle) return;
+    setSelected(selectedTitle);
+  };
+
+  return (
+    <StyledDropdown
+      onChange={handleSelect}
+      options={options}
+      defaultOption={selected}
+    />
+  );
+};
