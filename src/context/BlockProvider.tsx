9e797766da8de8157e9ac1d352e80cc4f6cfa2d5
James Lefrere <james.lefrere@mstable.org>
2021-04-22 14:53:42 +0200

    Per-network block times

--- src/context/BlockProvider.tsx
+++ src/context/BlockProvider.tsx
@@ -4,6 +4,7 @@ import { subDays, subHours } from 'date-fns'
 
 import { useBlockTimesForDates } from '../hooks/useBlockTimesForDates'
 import { useProvider, useIsIdle } from './AccountProvider'
+import { useNetwork } from './NetworkProvider'
 
 export type MaybeBlockNumber = number | undefined
 
@@ -20,6 +21,7 @@ const date24h = subHours(Date.now(), 24)
 
 export const BlockProvider: FC = ({ children }) => {
   const [blockNow, setBlockNow] = useState<MaybeBlockNumber>()
+  const network = useNetwork()
 
   const provider = useProvider()
   const idle = useIsIdle()
@@ -34,7 +36,7 @@ export const BlockProvider: FC = ({ children }) => {
         setBlockNow(latest ? parseInt((latest as unknown) as string, 10) : undefined)
       })
     }
-  }, 20000)
+  }, network.blockTime)
 
   return (
     <ctx.Provider
