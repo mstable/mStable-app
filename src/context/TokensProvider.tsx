ffccd6297bf8a895e591203ce7a52e1622d05fe8
James Lefrere <james.lefrere@mstable.org>
2021-04-22 20:27:45 +0200

    Remove POS prefix

--- src/context/TokensProvider.tsx
+++ src/context/TokensProvider.tsx
@@ -92,7 +92,7 @@ const reducer: Reducer<State, Action> = (state, action) => {
             [address]: {
               address,
               decimals,
-              symbol,
+              symbol: symbol.replace('(PoS) mStable USD', 'mUSD'),
               balance: new BigDecimal(0, decimals),
               allowances: {},
               totalSupply: BigDecimal.fromMetric(totalSupply),
