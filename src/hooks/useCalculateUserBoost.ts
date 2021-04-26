fd3b104d50e7042c44d1fd904286f8a5d5e116a5
James Lefrere <james.lefrere@mstable.org>
2021-04-26 19:01:07 +0200

    Lint

--- src/hooks/useCalculateUserBoost.ts
+++ src/hooks/useCalculateUserBoost.ts
@@ -1,26 +1,26 @@
-import { useMemo } from 'react';
-import { ADDRESSES } from '../constants';
-import { BoostedSavingsVaultState } from '../context/DataProvider/types';
-import { useTokenSubscription } from '../context/TokensProvider';
-import { calculateBoost, calculateBoostImusd, getCoeffs } from '../utils/boost';
+import { useMemo } from 'react'
 
-export const useCalculateUserBoost = (
-  vault?: BoostedSavingsVaultState,
-): number => {
-  const vMTA = useTokenSubscription(ADDRESSES.vMTA);
+import type { BoostedSavingsVaultState } from '../context/DataProvider/types'
+import { useTokenSubscription } from '../context/TokensProvider'
+import { useNetworkAddresses } from '../context/NetworkProvider'
+import { calculateBoost, calculateBoostImusd, getCoeffs } from '../utils/boost'
 
-  const vMTABalance = vMTA?.balance;
-  const rawBalance = vault?.account?.rawBalance;
+export const useCalculateUserBoost = (vault?: BoostedSavingsVaultState): number => {
+  const networkAddresses = useNetworkAddresses()
+  const vMTA = useTokenSubscription(networkAddresses.vMTA)
+
+  const vMTABalance = vMTA?.balance
+  const rawBalance = vault?.account?.rawBalance
 
   const boost = useMemo<number>(() => {
-    if (!vault) return 1;
-    const coeffs = getCoeffs(vault);
-    const { isImusd } = vault;
-    return isImusd || !coeffs
-      ? calculateBoostImusd(rawBalance, vMTABalance)
-      : calculateBoost(...coeffs, rawBalance, vMTABalance);
-  }, [rawBalance, vMTABalance, vault]);
+    if (!vault) return 1
+
+    const coeffs = getCoeffs(vault)
+    const { isImusd } = vault
+
+    return isImusd || !coeffs ? calculateBoostImusd(rawBalance, vMTABalance) : calculateBoost(...coeffs, rawBalance, vMTABalance)
+  }, [rawBalance, vMTABalance, vault])
 
   // Fallback to 1x multiplier
-  return boost ?? 1;
-};
+  return boost ?? 1
+}
