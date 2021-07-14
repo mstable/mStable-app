e74b3641004120a7c37f65fc2e71e8d54ab0a41f
James Lefrere <james.lefrere@mstable.org>
2021-07-14 11:27:39 +0200

    Skip feeders queries if needed
    - Skip feeder pools queries if the subgraph isn't defined on the current network

--- src/updaters/tokenFetcher.ts
+++ src/updaters/tokenFetcher.ts
@@ -4,6 +4,7 @@ import { AllTokensQuery, AllTokensQueryVariables, useAllTokensQuery as useAllTok
 import { useFeederTokensQuery, FeederTokensQuery, FeederTokensQueryVariables } from '../graphql/feeders'
 
 import { useTokensDispatch } from '../context/TokensProvider'
+import { useNetwork } from '../context/NetworkProvider'
 
 const options = {
   fetchPolicy: 'network-only',
@@ -13,10 +14,14 @@ const options = {
  * Updater to one-time fetch all ERC20 tokens from the subgraph.
  */
 export const TokenFetcher = (): null => {
+  const network = useNetwork()
   const { setFetched } = useTokensDispatch()
 
   const protocolQuery = useAllTokensProtocolQuery(options as QueryHookOptions<AllTokensQuery, AllTokensQueryVariables>)
-  const feedersQuery = useFeederTokensQuery(options as QueryHookOptions<FeederTokensQuery, FeederTokensQueryVariables>)
+  const feedersQuery = useFeederTokensQuery({
+    ...options,
+    skip: !Object.prototype.hasOwnProperty.call(network.gqlEndpoints, 'feeders'),
+  } as QueryHookOptions<FeederTokensQuery, FeederTokensQueryVariables>)
 
   const protocolFetched = protocolQuery.data?.tokens ?? []
   const feedersFetched = feedersQuery.data?.feederPools ?? []
